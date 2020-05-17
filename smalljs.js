1bb8ba6ce6e3457a12da0f9b2811100d14bbed6f
Shreyas Sreenivas <46835608+shreyas44@users.noreply.github.com>
2020-05-18 01:19:14 +0530

    Update smalljs.js

--- smalljs.js
+++ smalljs.js
@@ -1,24 +1,19 @@
-const int = val => parseInt(val);
-const float = val => parseFloat(val);
-const str = val => toString(val);
-const len = val => val.length
-const getIndex = (item, index) => item.search(index);
-const lower = val => val.toLowerCase();
-const upper = val => val.toUpperCase();
-const strip = val => val.trim();
-const round = (val, digits = 0) => val.toFixed(digits)
-const isArray = val => val instanceof Array
-const isNum = val => typeof val == 'number'
-const isStr = val => typeof val == 'string'
-const remUndefined = val => val.filter((value, index, array) => value != "undefined")
-const pop = (val, index) => val.splice(index, 1)
-const sorted = (val, reverse = false) => !reverse ? val.sort((a, b) => a - b) : val.sort((a, b) => b - a)
-const max = val => Math.max(...val)
-const min = val => Math.min(...val)
-const sum = val => val.reduce((total, value) => total + value, 0)
-const mean = val => sum(val) / len(val)
-
-
-
-
-//create your own sort function where you can provide a key
+const int = val => parseInt(val); // converts input to integer
+const float = val => parseFloat(val); // converts input to float
+const str = val => toString(val); // converts input to string
+const len = val => val.length // gets length of input, string or array
+const getIndex = (item, index) => item.search(index); // get index of item in array
+const lower = val => val.toLowerCase(); // converts string to lower case
+const upper = val => val.toUpperCase(); // converts string to upper case
+const strip = val => val.trim(); // strips the string of spaces in the beginning and end of the string
+const round = (val, digits = 0) => val.toFixed(digits) // round the input
+const isArray = val => val instanceof Array // checks if value is an array
+const isNum = val => typeof val == 'number' // checks if value is a number
+const isStr = val => typeof val == 'string' // checks if value is a string
+const remUndefined = val => val.filter((value, index, array) => value != "undefined") // removes all undefined objects from an array
+const pop = (val, index) => val.splice(index, 1) // remove item from array
+const sorted = (val, reverse = false) => !reverse ? val.sort((a, b) => a - b) : val.sort((a, b) => b - a) // sort items in array
+const max = val => Math.max(...val) // get max value in an array
+const min = val => Math.min(...val) // get min value in an array
+const sum = val => val.reduce((total, value) => total + value, 0) //get sum of all values in an array
+const mean = val => sum(val) / len(val) // get mean value of items in the array
